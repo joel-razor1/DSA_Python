820e9bf83538d68ff2b0b00f3905844c9370b3f5
joel-razor1 <joelabraham8857@gmail.com>
2019-03-01 19:15:45 +0530

    No idea on the approach yet

--- Week 4/poly.py
+++ Week 4/poly.py
@@ -1,30 +1,58 @@
 #practice assignment question2
 
 def addpoly(p1,p2):
-    p1c=[]
-    p2c=[]
-    p1e=[]
-    p2e=[]
-    result=[]
-    for ic in p1:
-        p1c.append(ic[0])
-    for ie in p1:
-        p1e.append(ie[1])
-    for jc in p2:
-        p2c.append(jc[0])
-    for je in p2:
-        p2e.append(je[1])
-    print(p1c)
-    print(p1e)
-    print(p2c)
-    print(p2e)      
-    for i in range(0,len(p1e)):
-        for j in range(0,len(p2e)):
-            if p1e[i]==p2e[j]:
-                result.append((p1c[i]+p2c[j],p1e[i]))
-            elif 
+    poly1_coeff=[]
+    poly2_coeff=[]
+    poly1_exp=[]
+    poly2_exp=[]
+    add_coeff=[]
+    count=0
+    flag=[]
 
-    return result    
+    #inserting the coeficients and lists into seperate lists.
+    for i in range(0,len(p1)):
+        poly1_coeff.append(p1[i][0])
+        poly1_exp.append(p1[i][1])
+    for j in range(0,len(p2)):
+        poly2_coeff.append(p2[j][0])
+        poly2_exp.append(p2[j][1])
+
+    #sorting the polynomial in higher order first
+    for i in range(0,len(poly1_exp)):
+        for j in range(0,len(poly1_exp)):
+            if poly1_exp[i]>poly1_exp[j]:
+                (poly1_exp[i],poly1_exp[j])=(poly1_exp[j],poly1_exp[i])
+                (poly1_coeff[i],poly1_coeff[j])=(poly1_coeff[j],poly1_coeff[i])
+
+    for i in range(0,len(poly2_exp)):
+        for j in range(0,len(poly2_exp)):
+            if poly2_exp[i]>poly2_exp[j]:
+                (poly2_exp[i],poly2_exp[j])=(poly2_exp[j],poly2_exp[i])
+                (poly2_coeff[i],poly2_coeff[j])=(poly2_coeff[j],poly2_coeff[i])
+
+    print(poly1_coeff)
+    print(poly1_exp)
+    print(poly2_coeff)
+    print(poly2_exp)
+    """            
+    for i in range(0,len(poly1_exp)):
+        for j in range(0,len(poly2_exp)):
+            if poly1_exp[i]==poly2_exp[j]:
+                count=count+1
+            if count==0:
+                flag.append(1)
+    print(flag)     
+    """
+    
+    #for pol1e in range(0,len(poly1_exp)):
+     #   for pol2e in range(0,len(poly2_exp)):
+      #      if poly1_exp[pol1e]==poly2_exp[pol2e]:
+       #         add_coeff.append(poly1_coeff[pol1e]+poly2_coeff[pol2e])
+
+    
+                
+
+
 
 
 
